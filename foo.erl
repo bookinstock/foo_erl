d68259cebab0380485c02cd769b885b7addad041
Wende Lu <wendelu1991@gmail.com>
2020-10-03 22:12:06 +0800

    greeting_and_double

--- foo.erl
+++ foo.erl
@@ -1,12 +1,16 @@
 -module(foo).
 
--export([hello/0, add/2]).
+-export([hello/0, add/2, greeting_and_double/1]).
 
--import(io, [format/1]).
+% -import(io, [format/1]).
+
+greeting_and_double(X):
+  hello(),
+  add(X, X).
 
 %% hello world
 hello() -> 
-  format("world~n").
+  io:format("world~n").
 
 add(A, B) ->
   A + B.
